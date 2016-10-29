c41a70a33334b22117d75b67eeaaa2c9a1c5c827
Guru Dhanush Pochineni <gurudhanush@outlook.com>
2016-10-30 01:31:23 +0530

    Added a new person class to get general data .

---
+++ src/com/BlanC/Person.java
@@ -0,0 +1,32 @@
+package com.BlanC;
+
+/**
+ * Created by Administrator on 30-10-2016.
+ */
+
+public class Person
+{
+    public Person()
+    {
+
+    }
+    public Person(String name,String surname,String address)
+    {
+        this.Name = name;
+        this.SurName = surname;
+        this.Address = address;
+    }
+
+    public String Name;
+    public String SurName;
+    public String Address;
+
+    public long PhoneNumber;
+    public  long MobileNumber;
+    public long AadharNumber;
+    public long PanCardNumber;
+    public String Bank;
+    public String Branch;
+    public long AccountNumber;
+    public long BankIFSCCode;
+}
