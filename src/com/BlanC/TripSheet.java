4c78fa49d61a8456d8d88a0d036dc6bf178fc10d
Mukund <vmukund13@gmail.com>
2016-11-14 00:55:14 +0530

    Modified Classes

--- src/com/BlanC/TripSheet.java
+++ src/com/BlanC/TripSheet.java
@@ -1,7 +1,33 @@
 package com.BlanC;
 
+import java.sql.Date;
+
 /**
  * Created by Mukund on 11/13/2016.
  */
-public class TripSheet {
+public class TripSheet
+{
+    private String Division ;
+    private String Section;
+    private String Village;
+    private int RyotCode ;
+    private String RyotName;
+    private Date Season;
+    private int CuttingOrderNum ;
+    private int PlotNum;
+    private String Plant;                       //ytm
+    private Date DateOfPlant;
+    private int Extent ;
+    private int EstimatedYield ;
+    private String Variety;
+    private String SubMestary;
+    private String Mestary;
+    private int HarvestingRate;
+    private String VehicleNum;
+    private String Transporter;
+
+
+
+
+
 }
